6e9b57d70e57c5dd8a5d8133c7c6106972e02432
Tim Holper <tholper@uchicago.edu>
2018-11-05 21:47:34 +0000

    msm_mon_2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105150130) do
+ActiveRecord::Schema.define(version: 20181105212624) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20181105150130) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.integer "movie_id"
+    t.integer "actor_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "directors", force: :cascade do |t|
